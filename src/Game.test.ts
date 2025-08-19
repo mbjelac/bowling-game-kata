5ec60ef835b71e885a9122c27866b7eb93e52a89
mbjelac <marko.bjelac@gmail.com>
2025-08-19 21:32:44 +0200

    Refactor

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -7,14 +7,6 @@ beforeEach(() => {
   game = new Game();
 });
 
-it("score is sum of knocked down pins", () => {
-
-  game.roll(2);
-  game.roll(3);
-
-  expect(game.getScore()).toBe(5);
-});
-
 it("can not roll more than 20 times", () => {
 
   Array(20)
@@ -26,11 +18,20 @@ it("can not roll more than 20 times", () => {
   expect(() => game.roll(1)).toThrow();
 });
 
-it("a spare bonus is next roll's score", () => {
-
-  game.roll(4);
-  game.roll(6);
-  game.roll(3);
-
-  expect(game.getScore()).toBe(16);
+it("score is sum of knocked down pins", () => {
+  whenRollsKnockDown(2, 3);
+  thenScoreIs(5);
 });
+
+it("a spare bonus is next roll's score", () => {
+  whenRollsKnockDown(4, 6, 3);
+  thenScoreIs(16);
+});
+
+function whenRollsKnockDown(...pins: number[]) {
+  pins.forEach(pin => game.roll(pin));
+}
+
+function thenScoreIs(expectedScore: number) {
+  expect(game.getScore()).toBe(expectedScore);
+}
