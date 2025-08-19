ade262facc9a9b3a07b04df758efaddafdbca488
mbjelac <marko.bjelac@gmail.com>
2025-08-19 21:13:15 +0200

    Refactor

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -2,7 +2,7 @@ import { Game } from "./Game";
 
 const game = new Game();
 
-it("score of one role is the sum of knocked down pins", () => {
+it("score is sum of knocked down pins", () => {
 
   game.roll(2);
   game.roll(3);
