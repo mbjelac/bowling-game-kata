5efe3ea1452af23f943d726a5a9ec5eec5332de8
mbjelac <marko.bjelac@gmail.com>
2025-09-04 16:16:07 +0200

    Add test

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,7 +1,41 @@
+import { Game } from "./Game";
+
+let game: Game;
+
+beforeEach(() => {
+  game = new Game();
+});
+
 describe("the game consists of 10 frames with 2 rolls each", () => {
 
-  it("", () => {
-    
+  it("can not roll more than 10 frames with no spares & no strikes", () => {
+    whenRollsKnockDown(
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+      1, 2,
+    );
+
+    thenNoMoreRollsAllowed();
   });
-  
+
 });
+
+function whenRollsKnockDown(...konckedDownPins: number[]) {
+  konckedDownPins.forEach(pins => {
+    game.roll(pins);
+  });
+}
+
+function thenNoMoreRollsAllowed() {
+  expect(
+    () => game.roll(1)
+  )
+  .toThrow();
+}
